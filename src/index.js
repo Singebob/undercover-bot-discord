ab270ef600220f6de0badf30be97648992ee1724
Julien pellat <julien.pellat@epitech.eu>
2020-03-28 14:29:06 +0100

    Add TS Template

---
+++ src/index.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var discord_js_1 = require("discord.js");
+var bot = discord_js_1["default"].Client();
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import discord from 'discord.js';
+const bot: any = discord.Client();
+
